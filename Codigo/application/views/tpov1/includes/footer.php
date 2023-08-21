bc902ff25e8b11c3e3f16cb923a5e2d7d27c3798
Teckwar160 <teckwar160@gmail.com>
2023-08-21 11:21:13 -0600

    Base de Publicidad Abierta

---
+++ Codigo/application/views/tpov1/includes/footer.php
@@ -0,0 +1,141 @@
+<?php
+
+?>
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /.content-wrapper -->
+		
+        <footer style="background: #333333; padding-bottom:10px;">
+            <div class="container">
+				<div class="row">
+                    <div class="col-md-4">
+                    <br><br><br>
+                    <ul>
+                        <a href="http://inicio.inai.org.mx" target="_blank">
+                            <img class="img-footer" src="<?php echo base_url() . 'dist/img/INAI.png' ?>"
+							onmouseout="this.src='<?php echo base_url() . 'dist/img/INAI.png' ?>';" 
+							onmouseover="this.src='<?php echo base_url() . 'dist/img/INAI_HOVER.png' ?>';"							
+							style="display:block; margin:auto; height:77px;"> 
+                        </a>
+                        <br>
+						<h5 class="ainstitucion" style="font-size:16px; text-align:center;">En colaboración con:</h5>
+						<br>
+                        <a href="http://fundar.org.mx/" target="_blank">
+                            <img class="img-footer" src="<?php echo base_url() . 'dist/img/fundar-footer-04.png' ?>"					
+							style="display:block; margin:auto;"> 
+                        </a>
+                    </ul>                         
+                    </div>
+                    <div class="col-md-4" style="text-align:center;">
+                        <h5 class="ainstitucion" style="font-size:16px; margin-top:41px;">Síguenos:</h5>
+						<table style="margin: 0 auto;">
+							<td>
+								<a href="https://www.facebook.com/INAImx/" target="_blank">
+								<img class="a-redes-footer" src="<?php echo base_url() . 'dist/img/Facebook.png' ?>"
+									onmouseout="this.src='<?php echo base_url() . 'dist/img/Facebook.png' ?>';" 
+									onmouseover="this.src='<?php echo base_url() . 'dist/img/facebook_hover.png' ?>';"
+									style="display:block; margin:auto; height:45px;">
+								</a>
+							</td>
+							<td>
+								<a href="https://twitter.com/INAImexico" target="_blank">
+								<img class="a-redes-footer" src="<?php echo base_url() . 'dist/img/Twitter.png' ?>"
+									onmouseout="this.src='<?php echo base_url() . 'dist/img/Twitter.png' ?>';" 
+									onmouseover="this.src='<?php echo base_url() . 'dist/img/Twitter_hover.png' ?>';" 
+									style="display:block; margin:auto; height:45px;">
+								</a>
+							</td>
+						</table>
+						<br>
+						<h5 style="color:#0277bd; font-size:16px; font-weight: bold;">#PublicidadAbierta</h5>
+						
+                        <h5 class="ainstitucion" style="font-size:16px;" >Usa y mejora esta herramienta:</h5>
+                        <a href="https://github.com/INAImexico" target="_blank">
+                            <img class="a-redes-footer" src="<?php echo base_url() . 'dist/img/GITHUB.png' ?>"
+							onmouseout="this.src='<?php echo base_url() . 'dist/img/GITHUB.png' ?>';" 
+							onmouseover="this.src='<?php echo base_url() . 'dist/img/GITHUB_HOVER.png' ?>';"							
+							style="display:block; margin:auto; height:45px;"/>
+                        </a>
+                    </div>
+                    
+                    <div class="col-md-4" style="text-align: center; margin-top: 41px;">
+                        <h5 style="color: #999999; font-size:16px; font-weight: bold;">Sitios de interés:</h5>
+							<a href="http://www.plataformadetransparencia.org.mx/web/guest/sac" target="_blank"><h6 style="font-size:13px;">Plataforma Nacional de Transparencia<h6></a>
+                            <a href="https://micrositios.inai.org.mx/gobiernoabierto/" target="_blank"><h6 style="font-size:13px;">Gobierno Abierto y Transparencia</h6></a>
+                            <a href="https://micrositios.inai.org.mx/plandai/" target="_blank"><h6 style="font-size:13px;">Plan Nacional de Socialización del Derecho de Acceso a la Información</h6></a></
+                        <br><br><br><br><br><br>
+                        <h5 style="color: #999999; font-size:16px; font-weight: bold; margin-top:12px;">Contacto:</h5>
+                        
+                            <h6 style="font-size:13px; color: #999999">Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales</h6>
+                            <h6 style="font-size:13px; color: #999999">Insurgentes Sur No. 3211 Col. Insurgentes Cuicuilco, Alcaldía Coyoacán, C.P. 04530, Ciudad de México, México</h6>
+                            <h6 style="font-size:13px; color: #999999">TELINAI 01800 8354324</h6>
+                            <h6 style="font-size:13px; color: #999999">publicidadabierta@inai.org.mx</h6>                      
+                    </div>
+                </div>
+				<br>				
+				<div class="bottomFooter col-md-12" style="text-align:center; color:#999999; margin-top: 2.9em;">
+                Publicidad Abierta 2.0.0 antes Transparencia en Publicidad Oficial. Es una plataforma de código abierto basada en el prototipo funcional desarrollado por el Gobierno del Estado de Oaxaca 2011 – 2016 y Fundar.
+				</div> 				
+            </div>			
+        </footer>
+  
+    <!-- ./wrapper -->
+    
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap-slider.js" type="text/javascript"></script>
+    <!-- DATA TABES SCRIPT -->
+    <script src="<?php echo base_url(); ?>plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+    <!-- SlimScroll -->
+    <script src="<?php echo base_url(); ?>plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <!-- FastClick -->
+    <script src='<?php echo base_url(); ?>plugins/fastclick/fastclick.min.js'></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url(); ?>dist/js/app.min.js" type="text/javascript"></script>
+    <!-- fullCalendar 2.2.5 -->
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min.js" type="text/javascript"></script>-->
+    <script src="<?php echo base_url(); ?>plugins/js/moment-2.7.0.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+    <!-- Page specific script -->
+    <!-- cK Editor -->
+    <script src="<?php echo base_url(); ?>plugins/ckeditor/ckeditor.js" type="text/javascript"></script>
+    <!-- datepicker -->
+    <script src="<?php echo base_url(); ?>plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>plugins/datepicker/datepicker3.css">
+    <!-- bootbox -->
+    <script src="<?php echo base_url();?>plugins/bootboxjs/bootbox.min.js"></script>
+    <!-- charts-->
+    <script src="<?php echo base_url(); ?>plugins/highcharts/highstock.js"></script>
+    <!--<script src="<?php echo base_url(); ?>plugins/highcharts/highcharts.js"></script>-->
+    <script src="<?php echo base_url(); ?>plugins/highcharts/sankey.js"></script>
+    <script src="<?php echo base_url(); ?>plugins/highcharts/highcharts-more.js"></script>
+    <script src="<?php echo base_url(); ?>plugins/highcharts/networkgraph.js"></script> 
+    <script src="<?php echo base_url(); ?>plugins/highcharts/data.js">></script>
+    <script src="<?php echo base_url(); ?>plugins/highcharts/drilldown.js"></script>
+    <script src="<?php echo base_url(); ?>plugins/highcharts/map.js"></script>
+
+    
+    <script>
+    $(function(){
+        $(window).on("scroll",function(){
+            if( $(window).scrollTop()>$(".navbar").height() && $(".navbar").is(".navbar-static-top")){
+                var e=$(".navbar").height();
+                $(".navbar").css("top",-1*e).removeClass("navbar-static-top").addClass("navbar-fixed-top").animate({top:0})
+            }
+
+            if($(window).scrollTop()<$(".navbar").height() && $(".navbar").is(".navbar-fixed-top")){
+                var e=$(".navbar").height();
+                $(".navbar").removeClass("navbar-fixed-top").addClass("navbar-static-top").animate();
+            }
+        });
+    });
+    </script>
+    <?php echo $scripts; ?>
+    
+</body>
+</html>
+        
---
+++ README.md
@@ -0,0 +1,20 @@
+# **Publicidad_abierta**
+## Actualización de la herramienta Transparencia en publicidad oficial
+
+```
+• Módulo back-end
+• Módulo fron-end
+```
+### Hardware
+```
+• Almacenamiento: 250GB o superior 
+• Memoria RAM: 4 Gigabytes o superior
+• Velocidad de procesador: 1.3 a 3.0 GHz o superior
+```
+### Software
+```
+• Sistema operativo: Linux Mint
+• Base de datos: MySQL
+• Servidor de aplicaciones: Apache 2.0
+• PHP: 5.6
+```
